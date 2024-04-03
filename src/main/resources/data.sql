d5342a38815b6e9f7a223e3dbe7caf7f8831b2e3
Berke <58724259+berke-grl@users.noreply.github.com>
2024-04-03 11:55:04 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,6 @@ INSERT INTO product (name, price, stock) VALUES ('Book', 18.95, 25);
 INSERT INTO product (name, price, stock) VALUES ('Ruler', 10.99, 44);
 
 INSERT INTO customer (username, password, role, email, address, phone_number)
-VALUES ('berke', '$2a$12$6TH8XRknbJOhqMAro/pP.uUF/hnAoqIYKp.4AwKh3KwHzw0vqS8GK', 'USER', 'brk@hotmail.com', 'Karakaş mahallesi Kırklareli/Merkez', '05245428722');
+VALUES ('berke', '$2a$12$6TH8XRknbJOhqMAro/pP.uUF/hnAoqIYKp.4AwKh3KwHzw0vqS8GK', 'USER', 'brk@hotmail.com', 'Sungur mahallesi Kırklareli/Inece', '05245428722');
 INSERT INTO customer (username, password, role, email, address, phone_number)
-VALUES ('aktan', '$2a$12$/P2cxcnBU0DCnp1MXLZSpumUGHKT1d5//cKq2jJCoCaXoMeqMvCZu', 'ADMIN', 'aktan@hotmail.com', 'Gul Sokak Istanbul/Maltepe', '05445438758');
+VALUES ('aktan', '$2a$12$/P2cxcnBU0DCnp1MXLZSpumUGHKT1d5//cKq2jJCoCaXoMeqMvCZu', 'ADMIN', 'aktan@hotmail.com', 'Zafer Sokak Istanbul/Maslak', '05445438758');
